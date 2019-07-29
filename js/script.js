6458cd3d89eb9b7a6056ea631c92bd2dc28a4009
AndrewArtistics <50923847+AndrewArtistics@users.noreply.github.com>
2019-07-28 20:46:43 -0700

    Cleaned up and replaced commments

--- js/script.js
+++ js/script.js
@@ -1,21 +1,8 @@
 /******************************************
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
+Andy Tuinstra
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
 
 //global variables
@@ -27,7 +14,7 @@ function showPage(list, page) {
     let startIndex = (page * studentPages) - studentPages;
     let endIndex = (page * studentPages);
     for (let i = 0; i < list.length; i += 1) {
-        if ([i] >= startIndex && [i] <= endIndex) {
+        if (i >= startIndex && i <= endIndex) {
             list[i].style.display = '';
         } else {
             list[i].style.display = 'none';
@@ -47,10 +34,13 @@ function appendPagesLinks(list) {
     //creates ul element and appends it to the div
     const ul = document.createElement('ul');
     div.appendChild(ul);
-
+    
+    //loops the new list and anchor elements for each page.
     for (let i = 1; i <= pageNum ; i += 1) {
+        //creates a list element and appends it to the ul.
         let li = document.createElement('li');
         ul.appendChild(li);
+        //creates an anchor and appends it to the list elements.
         let a = document.createElement('a');
         a.href = '#';
         a.textContent = [i];
@@ -60,7 +50,7 @@ function appendPagesLinks(list) {
         };
         li.appendChild(a);
 
-        //event listener to make pages appear when the anchor is clicked
+        //event listener to make pages 'appear' when the anchor button is clicked
         a.addEventListener('click', (e) =>{
             let activeP = document.querySelector('.active');
             activeP.classList.remove('active');
@@ -69,34 +59,7 @@ function appendPagesLinks(list) {
         });
     };
 };
+
+//calls functions to make them work/show up on the page properly
 showPage(students, 1);
 appendPagesLinks(students);
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
